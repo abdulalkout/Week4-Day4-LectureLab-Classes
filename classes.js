91ff2b9d01e440eaa4bfaab865044710e6364040
abdulalkout <abdul.alkout@gmail.com>
2023-10-27 08:46:48 -0500

    created hamster class

--- classes.js
+++ classes.js
@@ -1,2 +1,19 @@
 //-----------------------//
 //-------Classes---------//
+
+class Hamster {
+  constructor(name){
+    this.owner = ''
+    this.name = name
+    this.price = 15
+  }
+  wheelRun(){
+    console.log('squeak squeak')
+  }
+  eatFood(){
+    console.log('nibble nibble')
+  }
+  getPrice(){
+    return this.price
+  }
+}
